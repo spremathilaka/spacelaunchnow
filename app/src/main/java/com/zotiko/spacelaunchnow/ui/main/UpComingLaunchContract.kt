35ed4c54216994ff36fb5963dc5de599732dabea
samith <samith143@gmail.com>
2020-03-14 17:31:45 +1100

    add navigation arc library to handle the ui navigation

--- app/src/main/java/com/zotiko/spacelaunchnow/ui/main/UpComingLaunchContract.kt
+++ app/src/main/java/com/zotiko/spacelaunchnow/ui/main/UpComingLaunchContract.kt
@@ -1,13 +1,13 @@
 package com.zotiko.spacelaunchnow.ui.main
 
-import com.zotiko.spacelaunchnow.model.LaunchEvent
+import com.zotiko.spacelaunchnow.dto.LaunchEventDTO
 import com.zotiko.spacelaunchnow.ui.data.PageErrorState
 
 interface UpComingLaunchContract {
 
     data class ViewState(
         val isLoading: Boolean = false,
-        val activityData: List<LaunchEvent> = listOf(),
+        val activityData: List<LaunchEventDTO> = listOf(),
         val errorState: PageErrorState? = null
     )
 }
---
+++ app/src/main/java/com/zotiko/spacelaunchnow/ui/main/detail/DetailFragment.kt
@@ -0,0 +1,33 @@
+package com.zotiko.spacelaunchnow.ui.main.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.zotiko.spacelaunchnow.databinding.FragmentDetailBinding
+import com.zotiko.spacelaunchnow.dto.LaunchEventDTO
+
+class DetailFragment : Fragment() {
+
+    private lateinit var fragmentBinding: FragmentDetailBinding
+
+    private lateinit var selectedData: LaunchEventDTO
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val safeArgs: DetailFragmentArgs by navArgs()
+        selectedData = safeArgs.selectedData
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        fragmentBinding = FragmentDetailBinding.inflate(inflater, container, false)
+        fragmentBinding.uiData = selectedData
+        return fragmentBinding.root
+    }
+}
