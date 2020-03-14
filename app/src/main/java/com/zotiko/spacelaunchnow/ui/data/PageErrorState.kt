c742e3540dad14394d3e4c4654a85b7f68df1fef
samith <samith143@gmail.com>
2020-03-14 21:27:50 +1100

    rename util packages

--- app/src/main/java/com/zotiko/spacelaunchnow/ui/data/PageErrorState.kt
+++ app/src/main/java/com/zotiko/spacelaunchnow/ui/data/PageErrorState.kt
@@ -1,8 +1,8 @@
 package com.zotiko.spacelaunchnow.ui.data
 
 import com.zotiko.spacelaunchnow.R
-import com.zotiko.spacelaunchnow.ui.util.ResText
-import com.zotiko.spacelaunchnow.ui.util.Text
+import com.zotiko.spacelaunchnow.ui.util.text.ResText
+import com.zotiko.spacelaunchnow.ui.util.text.Text
 
 enum class PageErrorState(val message: Text) {
     NO_NETWORK(ResText(R.string.no_internet_error_msg)),
