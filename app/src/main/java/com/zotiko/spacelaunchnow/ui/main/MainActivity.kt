4f2c8c0a55540cce421581b37cba92f686b66519
samith <samith143@gmail.com>
2020-03-14 21:14:36 +1100

    enable back navigation

--- app/src/main/java/com/zotiko/spacelaunchnow/ui/main/MainActivity.kt
+++ app/src/main/java/com/zotiko/spacelaunchnow/ui/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.zotiko.spacelaunchnow.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.zotiko.spacelaunchnow.R
 import com.zotiko.spacelaunchnow.ui.upcominglaunches.list.LaunchEventListFragment
 import dagger.android.AndroidInjection
@@ -20,7 +22,17 @@ class MainActivity : AppCompatActivity(), HasAndroidInjector,
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        setupNavigation()
     }
 
+    private fun setupNavigation() {
+        val navController = Navigation.findNavController(this, R.id.nav_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
     override fun androidInjector(): AndroidInjector<Any> = androidInjector
+
+    override fun onSupportNavigateUp() =
+        Navigation.findNavController(this, R.id.nav_fragment).navigateUp()
+
+
 }
